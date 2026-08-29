173ab707c3e25ac065c87bfd8ca5ff9835438c98
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2026-08-29 14:10:47 +0300

    Calculate colors for characters

--- cmd/web/wordmaster/guess.go
+++ cmd/web/wordmaster/guess.go
@@ -3,6 +3,7 @@ package wordmaster
 import (
 	"fmt"
 	"net/http"
+	"slices"
 	"strings"
 )
 
@@ -17,7 +18,11 @@ func GuessHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received guess: ", guess)
 
 	solution, err := r.Cookie("solution")
-	fmt.Println("Solution is: ", solution)
+	fmt.Println("Solution is: ", solution.Value)
+
+	colors := getColorsForWord(strings.ToLower(guess), strings.ToLower(solution.Value))
+
+	fmt.Printf("Colors are: %v\n", colors)
 
 	// name := r.FormValue("name")
 	// component := HelloPost(name)
@@ -26,4 +31,41 @@ func GuessHandler(w http.ResponseWriter, r *http.Request) {
 	// 	http.Error(w, err.Error(), http.StatusBadRequest)
 	// 	log.Fatalf("Error rendering in HelloWebHandler: %e", err)
 	// }
+}
+
+type Color = string
+
+const (
+	White  Color = "white"
+	Yellow Color = "yellow"
+	Green  Color = "green"
+	Gray   Color = "gray"
+)
+
+func getColorsForWord(guess, solution string) []Color {
+	result := []Color{White, White, White, White, White}
+	characterPool := strings.Split(solution, "")
+
+	for i, c := range guess {
+		if c == rune(solution[i]) {
+			result[i] = Green
+			characterPool[i] = ""
+		}
+	}
+
+	for i, c := range guess {
+		if result[i] == Green {
+			continue
+		}
+
+		foundIndex := slices.Index(characterPool, string(c))
+		if foundIndex != -1 {
+			result[i] = Yellow
+			characterPool[foundIndex] = ""
+		} else {
+			result[i] = Gray
+		}
+	}
+
+	return result
 }
